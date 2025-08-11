36d09ada577995dc1f00341bed751b35bfe811c4
VicTheM <victorychibuike121@gmail.com>
2025-08-11 12:16:53 +0100

    Imported a package and use!

--- HelloWorld/hello_world.go
+++ HelloWorld/hello_world.go
@@ -2,9 +2,22 @@ package main
 
 /* This is a simple Go program that prints "Hello, World!" to the console. */
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
 	fmt.Println("Welcome to Go programming!")
+
+	// Using function from an external package
+	fmt.Println(quote.Hello())
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Println(quote.Go())
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Println(quote.Glass())
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Println(quote.Opt())
 }
