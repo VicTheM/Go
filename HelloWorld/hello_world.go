de31da371270c02d5ea58f042fa173b192f620f1
VicTheM <victorychibuike121@gmail.com>
2025-08-11 12:05:49 +0100

    Hello World in Go

---
+++ HelloWorld/hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+/* This is a simple Go program that prints "Hello, World!" to the console. */
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+	fmt.Println("Welcome to Go programming!")
+}
