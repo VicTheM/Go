b7cf0cad0e951f905f13d508c9a9ea906a8302a9
VicTheM <victorychibuike121@gmail.com>
2025-10-16 16:09:20 +0100

    Go routine test

--- TheGoProgrammingLanguagePdf/ChapterOne/go_routine/go_routine.go
+++ TheGoProgrammingLanguagePdf/ChapterOne/go_routine/go_routine.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// start := time.Now()
-	// fmt.Println("Main function started")
-	// go routine()
-
-	// time.Sleep(1 * time.Second)
-	for x := 0; x < 1000000; x++ {
-		go fmt.Println("rotine iteration: ", x, "")
-	}
-
-	// secs := time.Since(start).Seconds()
-	// fmt.Println("secs used: ", secs)
 }
-
-// func routine() {
-// 	for x := 0; x < 10; x++ {
-// 		fmt.Println("rotine iteration: ", x)
-// 	}
-// }
